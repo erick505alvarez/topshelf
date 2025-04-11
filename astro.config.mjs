f01d243a93f7ce4e9f82ffede8489e4885136b6b
Erick Alvarez <erick505alvarez@gmail.com>
2025-04-10 21:45:18 -0500

    Move to healthbodybalance.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://topshelf.totalhealthandwellness.org",
+  site: "https://topshelf.healthbodybalance.org",
   base: "/topshelf",
 });
