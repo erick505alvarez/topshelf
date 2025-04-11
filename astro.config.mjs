a0c4db3ebeefed8d315b4c5a6ba554fbc120e683
Erick Alvarez <erick505alvarez@gmail.com>
2025-04-10 21:57:23 -0500

    Update astro.config.mjs base path and site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  // site: "https://topshelf.healthbodybalance.org",
+  // base: "/topshelf/",
+
   site: "https://topshelf.healthbodybalance.org",
-  base: "/topshelf",
+  base: "/",
 });
