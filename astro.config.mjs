59e4bec5cb9ad581bca5cfb5d424e3070899bdf8
Erick Alvarez <erick505alvarez@gmail.com>
2025-04-10 20:03:46 -0500

    Add /topshelf base path to internal links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: "https://erick505alvarez.github.io",
+  base: "/topshelf",
 });
